195a7a950d636ce615cea6ad17ee15296cf1c5b5
scott <scottchowww@gmail.com>
2023-07-20 14:32:39 +0800

    add ft_strrchr

---
+++ circle-00/libft/ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scott <scottchowww@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 14:15:28 by scott             #+#    #+#             */
+/*   Updated: 2023/07/20 14:30:52 by scott            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	while (i != 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i--;
+	}
+	if (s[i] == (char)c)
+		return ((char *)s + i);
+	return (NULL);
+}
