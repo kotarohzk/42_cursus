e74dd8d4d06c842e84b0642dff846f7cd0e67d94
scott <scottchowww@gmail.com>
2023-07-20 14:12:05 +0800

    add ft_strchr

--- circle-00/libft/libft.h
+++ circle-00/libft/libft.h
@@ -9,3 +9,4 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
