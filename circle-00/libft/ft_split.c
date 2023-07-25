9dd78137af22c9530fec21c2cb88d3f14d67ff0d
scott <scottchowww@gmail.com>
2023-07-25 12:44:43 +0800

    add ft_split

---
+++ circle-00/libft/ft_split.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scott <scottchowww@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 10:26:02 by scott             #+#    #+#             */
+/*   Updated: 2023/07/25 12:42:15 by scott            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+static int	get_number_of_elements(char const *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (*s)
+	{
+		if (*s == c)
+			i++;
+		s++;
+	}
+	return (i + 1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**buf_array;
+	size_t	i;
+	size_t	j;
+
+	if (!s)
+		return (NULL);
+	buf_array = malloc(sizeof(char *) * get_number_of_elements(s, c));
+	if (!buf_array)
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (s[++i])
+	{
+		if (s[i] == c)
+		{
+			buf_array[j] = ft_substr(s, 0, i);
+			s = s + i + 1;
+			i = -1;
+			j++;
+		}
+	}
+	buf_array[j] = ft_substr(s, 0, ft_strlen(s));
+	return (buf_array);
+}
