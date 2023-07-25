d8b734547cf97f641c86adbc6d25d9a098dcf7b9
scott <scottchowww@gmail.com>
2023-07-25 10:24:15 +0800

    add ft_strtrim

---
+++ circle-00/libft/ft_strtrim.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scott <scottchowww@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 18:46:05 by scott             #+#    #+#             */
+/*   Updated: 2023/07/25 10:23:21 by scott            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strtrim(char const *s1, char const	*set)
+{
+	size_t	i;
+
+	if (!s1)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i - 1]))
+		i--;
+	return (ft_substr(s1, 0, i));
+}
