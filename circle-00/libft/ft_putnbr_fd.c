06fd19fc42a464b1730caeecfdd3a6d36bb6ef58
scott <scottchowww@gmail.com>
2023-07-25 18:16:44 +0800

    refactor code

--- circle-00/libft/ft_putnbr_fd.c
+++ circle-00/libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: scott <scottchowww@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 17:01:50 by scott             #+#    #+#             */
-/*   Updated: 2023/07/25 18:07:30 by scott            ###   ########.fr       */
+/*   Updated: 2023/07/25 18:15:04 by scott            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,6 @@ static int	ft_abs(int n)
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	sign;
-
-	sign = 0;
-	if (n < 0)
-		sign = 1;
 	if (n >= 10 || n <= -10)
 	{
 		ft_putnbr_fd((n / 10), fd);
@@ -33,7 +28,7 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n < 10 && n > -10)
 	{
-		if (sign)
+		if (n < 0)
 			ft_putchar_fd('-', fd);
 		ft_putchar_fd((ft_abs(n % 10) + 48), fd);
 	}
