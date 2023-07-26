35940f5323ec217cb38cd19612014fbfdf5761b5
scott <scottchowww@gmail.com>
2023-07-26 12:25:05 +0800

    add ft_lstdelone

--- circle-00/libft/libft.h
+++ circle-00/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: scott <scottchowww@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 22:07:19 by scott             #+#    #+#             */
-/*   Updated: 2023/07/26 11:15:32 by scott            ###   ########.fr       */
+/*   Updated: 2023/07/26 12:24:53 by scott            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,4 +57,5 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
