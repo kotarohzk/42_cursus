9b1975e7a528b17a6811e2603e63d0d6fbe90379
scott <scottchowww@gmail.com>
2023-07-26 12:59:23 +0800

    add ft_lstclear

---
+++ circle-00/libft/ft_lstclear.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scott <scottchowww@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 12:25:21 by scott             #+#    #+#             */
+/*   Updated: 2023/07/26 12:30:49 by scott            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	temp = NULL;
+	while (*lst)
+	{
+		temp = *lst;
+		*lst = (*lst)->next;
+		ft_lstdelone(temp, del);
+	}
+}
